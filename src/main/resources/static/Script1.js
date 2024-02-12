4a979c05e6acc5c2d18197052ec1db3b67d1d9ea
Vigneshwaran <125852792+vigneshwaranbalamurugan@users.noreply.github.com>
2024-02-12 22:00:56 +0530

    Updated Controller

--- src/main/resources/static/Script1.js
+++ src/main/resources/static/Script1.js
@@ -9,3 +9,27 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+function setUpdate(){
+    document.getElementById('updatepage').style.display="flex";
+    document.getElementById('homepage').style.display="none";
+    document.getElementById('votepage').style.display="none";
+}
+
+function setHome(){
+    document.getElementById('homepage').style.display="flex";
+    document.getElementById('updatepage').style.display="none";
+    document.getElementById('votepage').style.display="none";
+}
+
+function setVote(){
+    document.getElementById('votepage').style.display='flex';
+    document.getElementById('updatepage').style.display="none";
+    document.getElementById('homepage').style.display="none";
+
+}
+
+document.getElementById('update').addEventListener('click', () => setUpdate());
+document.getElementById('home').addEventListener('click', () => setHome());
+document.getElementById('vote').addEventListener('click',()=> setVote());
